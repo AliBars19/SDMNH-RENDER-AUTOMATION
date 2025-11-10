914d841f6b1ad9889ef607f4f8d0eb63273c1e68
AliBars19 <Ali.Bars@city.ac.uk>
2025-11-10 10:12:10 +0000

    parse yaml & check keys

--- update_db.py
+++ update_db.py
@@ -2,3 +2,29 @@ import os
 import json
 import yaml
 
+config = "config.yaml"
+
+if not os.path.exists(config):
+    raise FileNotFoundError(f"your config file has been moved or deleted")
+
+with open(config,'r') as f:
+    cfg = yaml.load(f, Loader=yaml.SafeLoader)
+
+db_path = cfg.get("db_path", "videodb.json")
+
+configkeys = ['channels','download_path','output_path','db_path','topics','target_seconds','yt_dlp_format']
+for key in config:
+    if key not in config:
+        raise KeyError(f"not all keys are in config.yaml, you might have accidentally deleted one")
+    
+
+
+# if os.path.exists(db_path):
+#     with open(db_path, 'r', encoding='utf-8') as f:
+#         try:
+#             data = json.load(f)
+#         except:
+#             print("data is corrupted or dont exist")
+#             data = []
+# else:
+#     data = []
