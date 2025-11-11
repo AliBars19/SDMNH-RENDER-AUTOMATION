1c15fcd3f490a34ea34f701277686fa00c996ea7
AliBars19 <Ali.Bars@city.ac.uk>
2025-11-11 16:12:57 +0000

    Final Push, Does not upload to YT succesffuly, need to investigate but video compilation works

--- main.py
+++ main.py
@@ -14,10 +14,8 @@ def authenticate_youtube():
     if os.path.exists(TOKEN_FILE):
         os.remove(TOKEN_FILE)
 
-    # Load client secrets file, put the path of your file
     client_secrets_file = "client.json"
 
-    
     flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
         client_secrets_file, SCOPES)
     credentials = flow.run_local_server()
@@ -27,38 +25,49 @@ def authenticate_youtube():
 
     return youtube
 
+
 def upload_video(youtube):
     request_body = {
         "snippet": {
             "categoryId": "22",
             "title": "Uploaded from Python",
-            "description": "This is the most awsome description ever",
-            "tags": ["test","python", "api" ]
+            "description": "This is the most awesome description ever",
+            "tags": ["test", "python", "api"]
         },
-        "status":{
+        "status": {
             "privacyStatus": "private"
         }
     }
 
-    # put the path of the video that you want to upload
     media_file = "outputs/among_us_compilation_2025-11-10_23-19-51.mp4"
+
+    media = googleapiclient.http.MediaFileUpload(
+        media_file,
+        mimetype="video/mp4",
+        chunksize=8 * 1024 * 1024,
+        resumable=True
+    )
 
     request = youtube.videos().insert(
         part="snippet,status",
         body=request_body,
-        media_body=googleapiclient.http.MediaFileUpload(media_file, chunksize=-1, resumable=True)
+        media_body=media
     )
 
-    response = None 
-
+    response = None
     while response is None:
         status, response = request.next_chunk()
         if status:
-            print(f"Upload {int(status.progress()*100)}%")
+            print(f"Upload {int(status.progress() * 100)}%")
 
-        print(f"Video uploaded with ID: {response['id']}")
+        if response is not None:
+            print(f"Video uploaded with ID: {response['id']}")
+
 
 if __name__ == "__main__":
     youtube = authenticate_youtube()
     upload_video(youtube)
 
+
+#giving up for now
+#but we will be back
